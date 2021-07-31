b7094132d398fa13bb54da75cb98b0bb66dcae75
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-07-31 16:48:25 -0300

    Removing pamac install commands of system-install

--- system-install.sh
+++ system-install.sh
@@ -13,11 +13,6 @@ then
 	systemctl enable NetworkManager
 	systemctl enable lightdm
 
-	pacman -S git
-	rm -rf /tmp/pamac-install
-	git clone https://aur.archlinux.org/pamac-aur.git /tmp/pamac-install
-	makepkg -sic BUILDDIR=/tmp/pamac-install
-
 	echo "Finalizado"
 else
 	echo "Não foi possível achar o arquivo packages. Saindo por segurança"
