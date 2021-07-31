ae85288b9155a1317527d497443c6b2bbef04fd3
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-07-31 15:59:09 -0300

    Added script packages-install.sh

---
+++ packages-install.sh
@@ -0,0 +1,18 @@
+DIRNAME=$(dirname $0)
+
+loadedPackages=$(cat $DIRNAME/packages >/dev/null && echo 1 || echo 0)
+
+if [ $loadedPackages = "1" ]
+then	
+	packages=($(cat packages))
+	pacman -Syu
+	pacman -S $packages --no-confirm
+
+	pacman -S git --no-confirm
+	git clone https://aur./archlinux.org/pamac-aur.git /tmp/pamac-install
+	makepkg -sic BUILDDIR=/tmp/pamac-install
+
+	echo "Finalizado"
+else
+	echo "Não foi possível achar o arquivo packages. Saindo por segurança"
+fi
